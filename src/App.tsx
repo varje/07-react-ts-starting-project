48955fd5542c578092bd27608cba5700557fcbea
varje <varjekivila@gmail.com>
2025-12-26 16:58:19 +0200

    add removing todos

--- src/App.tsx
+++ src/App.tsx
@@ -11,10 +11,15 @@ function App() {
       return prevTodos.concat(newTodo);
     });
   };
+  const removeToDoHandler = (todoId: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== todoId);
+    });
+  };
   return (
     <div>
       <NewTodo onAddTodo={addTodoHandler} />
-      <Todos items={todos} />
+      <Todos items={todos} onRemoveTodo={removeToDoHandler} />
     </div>
   );
 }
