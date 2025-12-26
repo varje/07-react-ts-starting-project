f2e9249fecbb7fa99256191783c1b7d5af545b60
varje <varjekivila@gmail.com>
2025-12-26 15:35:46 +0200

    add a data model for todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Todos from './components/Todos';
+import Todo from './models/todo';
 
 function App() {
+  const todos = [new Todo('Learn React'), new Todo('Learn TypeScript')];
   return (
     <div>
-      <Todos items={['item1', 'item2']} />
+      <Todos items={todos} />
     </div>
   );
 }
